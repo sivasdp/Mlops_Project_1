2048052b7f8795f4b7e4c05a4063e854cdc7ffce
sivasdp <sdpsiva191@gmail.com>
2024-02-11 18:30:48 +0530

    New 1

--- src/components/data_transorformation.py
+++ src/components/data_transorformation.py
@@ -1,62 +1,80 @@
 import sys
 from dataclasses import dataclass
-import numpy as np
+
+import numpy as np 
 import pandas as pd
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder,StandardScaler
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-import os
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
 from src.exception import CustomException
 from src.logger import logging
+import os
 
 from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-    preprocessor_file_path=os.path.join('artifact','preprocessor.pkl')
+    preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
 
-class DataTransformaiton:
+class DataTransformation:
     def __init__(self):
-        self.data_transform_config=DataTransformationConfig()
-    
-    def get_data_transformer_obj(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+    def get_data_transformer_object(self):
         '''
-        This function responsible for data tranformation
+        This function si responsible for data trnasformation
+        
         '''
         try:
-            numerical_columns=['writing_score','reading_score']
-            categorical_columns=['gender', 'race_ethnicity', 'parental_level_of_education', 'lunch', 'test_preparation_course']
-            num_pipeline=Pipeline(
+            numerical_columns = ["writing_score", "reading_score"]
+            categorical_columns = [
+                "gender",
+                "race_ethnicity",
+                "parental_level_of_education",
+                "lunch",
+                "test_preparation_course",
+            ]
+
+            num_pipeline= Pipeline(
                 steps=[
-                    ("imputer",SimpleImputer(strategy="median")),
-                    ("scaler",StandardScaler())
+                ("imputer",SimpleImputer(strategy="median")),
+                ("scaler",StandardScaler())
+
                 ]
             )
-            logging.info("Numerical coloumns encoding completed")
+
             cat_pipeline=Pipeline(
+
                 steps=[
-                    ("imputer",SimpleImputer(strategy="most_frequent")),
-                    ("one_hot_encoder",OneHotEncoder()),
-                    ("scaler",StandardScaler())
+                ("imputer",SimpleImputer(strategy="most_frequent")),
+                ("one_hot_encoder",OneHotEncoder()),
+                ("scaler",StandardScaler(with_mean=False))
                 ]
+
             )
-            logging.info("Categorical coloumns encoding completed")
+
+            logging.info(f"Categorical columns: {categorical_columns}")
+            logging.info(f"Numerical columns: {numerical_columns}")
 
             preprocessor=ColumnTransformer(
                 [
-                    ("num_pipeline",num_pipeline,numerical_columns),
-                    ("cat_pipeline",cat_pipeline,categorical_columns)
+                ("num_pipeline",num_pipeline,numerical_columns),
+                ("cat_pipelines",cat_pipeline,categorical_columns)
+
                 ]
+
+
             )
 
             return preprocessor
-
-
+        
         except Exception as e:
             raise CustomException(e,sys)
         
     def initiate_data_transformation(self,train_path,test_path):
+
         try:
             train_df=pd.read_csv(train_path)
             test_df=pd.read_csv(test_path)
@@ -92,7 +110,7 @@ class DataTransformaiton:
 
             save_object(
 
-                file_path=self.data_transform_config.preprocessor_file_path,
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
 
             )
@@ -100,14 +118,7 @@ class DataTransformaiton:
             return (
                 train_arr,
                 test_arr,
-                self.data_transform_config.preprocessor_file_path
+                self.data_transformation_config.preprocessor_obj_file_path,
             )
-
         except Exception as e:
             raise CustomException(e,sys)
-
-
-
-
-
-
